45846f53a8b2295c0d3b0df82c3b2a6b7ec6db33
citrus09 <citrus09@users.noreply.github.com>
2018-03-25 01:16:48 +0530

    adding more

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,8 +1,12 @@
 from sklearn import linear_model
 reg = linear_model.LinearRegression()
-reg.fit([[0,0], [1,1], [2,2]], [0,1,2])
+X = [[0,0], [1,1], [2,2]]
+y = [0,1,2]
+reg.fit(X, y)
 print(reg)
 print(reg.coef_)
 print(reg.intercept_)
 
-print(reg.predict([[5, 5]]))
+test = [[5, 5]]
+print(reg.predict(test))
+print(reg.score(X, y))
